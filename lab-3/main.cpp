cd70c7196cb80acfeed64b859d7289ef0fc2240d
915_Nagy_Matyas <tomesjerry3@gmail.com>
2023-10-27 14:43:50 +0300

    Batching

--- lab-3/main.cpp
+++ lab-3/main.cpp
@@ -4,6 +4,9 @@
 #include <string>
 #include <iostream>
 #include <mutex>
+#include <chrono>
+using namespace std::chrono;
+#define clock high_resolution_clock::now
 
 typedef std::vector<std::vector<int>> Matrix;
 
@@ -21,6 +24,17 @@ void calculateProduct(int i, int j) {
 		sum += a[i][k] * b[k][j];
 	}
 	result[i][j] = sum;
+}
+
+void startTask(int iStart, int iEnd) {
+	for (int i = iStart; i < iEnd; i++) {
+		if (i >= b.size()) {
+			break;
+		}
+		for (int j = 0; j < b[0].size(); j++) {
+			calculateProduct(i, j);
+		}
+	}
 }
 
 int main(int nrArgs, char* args[]) {
@@ -44,25 +58,21 @@ int main(int nrArgs, char* args[]) {
 	a = Matrix(n, std::vector<int>(m, 1));
 	b = Matrix(m, std::vector<int>(n, 1));
 
-	std::cout << "Matrix A: " << std::endl;
 	for (auto& row : a) {
 		for (auto& element : row) {
 			element = rand() % 10 + 1;
-			std::cout << element << " ";
 		}
-		std::cout << std::endl;
 	}
 
-	std::cout << "Matrix B: " << std::endl;
 	for (auto& row : b) {
 		for (auto& element : row) {
 			element = rand() % 10 + 1;
-			std::cout << element << " ";
 		}
-		std::cout << std::endl;
 	}
 
 	result = Matrix(n, std::vector<int>(n, 0));
+
+	auto startTime = clock();
 
 	auto threads = std::vector<std::thread>();
 
@@ -76,10 +86,52 @@ int main(int nrArgs, char* args[]) {
 		thread.join();
 	}
 
-	for (auto& row : result) {
-		for (auto& element : row) {
-			std::cout << element << " ";
-		}
-		std::cout << std::endl;
+	auto endTime = clock();
+
+	int ms = duration_cast<milliseconds>(endTime - startTime).count();
+
+	std::cout << "Time to calculate product for " << n << "x" << m << " matrix:" << ms << "ms" << std::endl;
+
+	// std::cout << "Matrix A: " << std::endl;
+	// for (auto& row : a) {
+	// 	for (auto& element : row) {
+	// 		std::cout << element << " ";
+	// 	}
+	// 	std::cout << std::endl;
+	// }
+
+	// std::cout << "Matrix B: " << std::endl;
+	// for (auto& row : b) {
+	// 	for (auto& element : row) {
+	// 		std::cout << element << " ";
+	// 	}
+	// 	std::cout << std::endl;
+	// }
+
+	// for (auto& row : result) {
+	// 	for (auto& element : row) {
+	// 		std::cout << element << " ";
+	// 	}
+	// 	std::cout << std::endl;
+	// }
+
+	result = Matrix(n, std::vector<int>(n, 0));
+
+	startTime = clock();
+
+	threads = std::vector<std::thread>();
+
+	for (int i = 0; i < n; i+=2) {
+		threads.emplace_back(startTask, i, i + 2);
 	}
+
+	for (auto& thread : threads) {
+		thread.join();
+	}
+
+	endTime = clock();
+
+	ms = duration_cast<milliseconds>(endTime - startTime).count();
+
+	std::cout << "Time to calculate product for " << n << "x" << m << " matrix with batching: `" << ms << " ms`" << std::endl;
 }
---
+++ lab-3/times.md
@@ -0,0 +1,17 @@
+# Times
+
+## Unique thread for each element
+
+- Time to calculate product for 5x3 matrix: `2ms`
+- Time to calculate product for 10x10 matrix: `8ms`
+- Time to calculate product for 50x50 matrix: `284ms`
+- Time to calculate product for 100x100 matrix: `1072ms`
+- Time to calculate product for 250x250 matrix: `8437ms`
+
+## Batching each 2 row
+
+- Time to calculate product for 5x3 matrix with batching: `0 ms`
+- Time to calculate product for 10x10 matrix with batching: `0 ms`
+- Time to calculate product for 50x50 matrix with batching: `2 ms`
+- Time to calculate product for 100x100 matrix with batching: `11 ms`
+- Time to calculate product for 250x250 matrix with batching: `117 ms`
